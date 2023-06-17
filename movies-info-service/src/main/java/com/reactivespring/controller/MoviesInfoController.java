09811102a7f96c75619e101b46692a915c8b6465
Tobias <tobiasocchi03@hotmail.com>
2023-06-17 12:29:55 -0300

    added MovieInfo controller,service and curl command notes

---
+++ movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
@@ -0,0 +1,27 @@
+package com.reactivespring.controller;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.service.MovieInfoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/v1")
+public class MoviesInfoController {
+
+    private MovieInfoService movieInfoService;
+
+
+    public MoviesInfoController(MovieInfoService movieInfoService) {
+        this.movieInfoService = movieInfoService;
+    }
+
+    @PostMapping("/movieinfos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfo movieInfo) {
+
+        return movieInfoService.addMovieInfo(movieInfo).log();
+
+    }
+}
---
+++ movies-info-service/src/main/java/com/reactivespring/service/MovieInfoService.java
@@ -0,0 +1,21 @@
+package com.reactivespring.service;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.repository.MovieInfoRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class MovieInfoService {
+
+    private MovieInfoRepository movieInfoRepository;
+
+    public MovieInfoService(MovieInfoRepository movieInfoRepository) {
+        this.movieInfoRepository = movieInfoRepository;
+    }
+
+    public Mono<MovieInfo> addMovieInfo(MovieInfo movieInfo) {
+
+        return movieInfoRepository.save(movieInfo);
+    }
+}
