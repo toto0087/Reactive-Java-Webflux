fc0d80e77978423dc516c42657ec6c5912648995
Tobias <tobiasocchi03@hotmail.com>
2023-07-02 12:56:09 -0300

    added: intg test for post method,get all endpoint + intg test,get endpoint by id + intg test, put endpoint + intg test

--- movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
+++ movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
@@ -4,6 +4,7 @@ import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.service.MovieInfoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -17,11 +18,25 @@ public class MoviesInfoController {
         this.movieInfoService = movieInfoService;
     }
 
+
+    @GetMapping("/movieinfos")
+    public Flux<MovieInfo> getAllMovieInfos() {
+        return movieInfoService.getAllMovies().log();
+    }
+
+    @GetMapping("/movieinfos/{id}")
+    public Mono<MovieInfo> getMovieInfos(@PathVariable String id) {
+        return movieInfoService.getMovie(id);
+    }
+
     @PostMapping("/movieinfos")
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfo movieInfo) {
-
         return movieInfoService.addMovieInfo(movieInfo).log();
+    }
 
+    @PutMapping("/movieinfos/{id}")
+    public Mono<MovieInfo> updateMovieInfo(@RequestBody MovieInfo updatedMovieInfo,@PathVariable String id) {
+        return movieInfoService.updateMovieInfo(updatedMovieInfo,id).log();
     }
 }
--- movies-info-service/src/main/java/com/reactivespring/service/MovieInfoService.java
+++ movies-info-service/src/main/java/com/reactivespring/service/MovieInfoService.java
@@ -3,6 +3,8 @@ package com.reactivespring.service;
 import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.repository.MovieInfoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -17,5 +19,27 @@ public class MovieInfoService {
     public Mono<MovieInfo> addMovieInfo(MovieInfo movieInfo) {
 
         return movieInfoRepository.save(movieInfo);
+    }
+
+    public Flux<MovieInfo> getAllMovies() {
+
+        return movieInfoRepository.findAll();
+    }
+
+    public Mono<MovieInfo> getMovie(String id) {
+
+        return movieInfoRepository.findById(id);
+    }
+
+    public Mono<MovieInfo> updateMovieInfo(MovieInfo updatedMovieInfo, String id) {
+
+        return movieInfoRepository.findById(id).
+                flatMap(movieInfo -> {
+                    movieInfo.setCast(updatedMovieInfo.getCast());
+                    movieInfo.setName(updatedMovieInfo.getName());
+                    movieInfo.setRelease_date(updatedMovieInfo.getRelease_date());
+                    movieInfo.setYear(updatedMovieInfo.getYear());
+                    return movieInfoRepository.save(movieInfo);
+                });
     }
 }
