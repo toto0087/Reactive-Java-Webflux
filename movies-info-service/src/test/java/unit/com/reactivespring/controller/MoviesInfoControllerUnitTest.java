17046457228586eb4f9fec9d39e8d4ffbb465731
Tobias <tobiasocchi03@hotmail.com>
2023-07-02 18:52:43 -0300

    Unit Test for getAll & getById

--- movies-info-service/src/test/java/unit/com/reactivespring/controller/MoviesInfoControllerUnitTest.java
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/MoviesInfoControllerUnitTest.java
@@ -1,11 +1,21 @@
 package com.reactivespring.controller;
 
+import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.service.MovieInfoService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
 
 @WebFluxTest(controllers = MoviesInfoController.class)
 @AutoConfigureWebTestClient
@@ -17,4 +27,45 @@ public class MoviesInfoControllerUnitTest {
     @MockBean
     private MovieInfoService movieInfoService;
 
+    static String MOVIES_INFO_URL = "/v1/movieinfos";
+
+    @Test
+    void getAllMovieInfos() {
+
+        var movieinfos = List.of(new MovieInfo(null, "Batman Begins",
+                        "2005", List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
+                new MovieInfo(null, "The Dark Knight",
+                        "2008", List.of("Christian Bale", "HeathLedger"), LocalDate.parse("2008-07-18")),
+                new MovieInfo("abc", "Dark Knight Rises",
+                        "2012", List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20")));
+
+        when(movieInfoService.getAllMovies()).thenReturn(Flux.fromIterable(movieinfos));
+
+        webTestClient
+                .get()
+                .uri(MOVIES_INFO_URL)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(MovieInfo.class)
+                .hasSize(3);
+    }
+
+    @Test
+    void getMovie() {
+        var movieInfoId = "abc";
+
+        when(movieInfoService.getMovie(isA(String.class)))
+                .thenReturn(Mono.just(new MovieInfo("abc", "Dark Knight Rises",
+                        "2012", List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20"))));
+
+        webTestClient
+                .get()
+                .uri(MOVIES_INFO_URL+"/{id}",movieInfoId)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody()
+                .jsonPath("$.name").isEqualTo("Dark Knight Rises");;
+    }
 }
