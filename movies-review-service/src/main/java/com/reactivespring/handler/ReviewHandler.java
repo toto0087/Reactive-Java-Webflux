933d3623e811bbe90897f2177270a9d6de2d7851
Tobias <tobiasocchi03@hotmail.com>
2023-07-05 16:46:38 -0300

    Functional post method + Intg test

--- movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
+++ movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
@@ -1,21 +1,25 @@
 package com.reactivespring.handler;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RouterFunction;
+import com.reactivespring.domain.Review;
+import com.reactivespring.repository.ReviewReactiveRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
-@Configuration
+@Component
 public class ReviewHandler {
 
-    @Bean
-    public RouterFunction<ServerResponse> reviewsRoute() {
+    private ReviewReactiveRepository reviewReactiveRepository;
 
-        return route()
-                .GET("/v1/helloworld",(request -> ServerResponse.ok().bodyValue("helloworld")))
-                .build();
+    public ReviewHandler(ReviewReactiveRepository reviewReactiveRepository) {
+        this.reviewReactiveRepository = reviewReactiveRepository;
+    }
+
+    public Mono<ServerResponse> addReview(ServerRequest request) {
+        return request.bodyToMono(Review.class)
+                .flatMap(reviewReactiveRepository::save)
+                .flatMap(ServerResponse.status(HttpStatus.CREATED)::bodyValue);
     }
 }
