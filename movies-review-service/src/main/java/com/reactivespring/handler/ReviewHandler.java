1f535243591d0f96e86e7249aa082c77b15a4a85
Tobias <tobiasocchi03@hotmail.com>
2023-07-05 11:03:14 -0300

    reviewRepository config

--- movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
+++ movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
@@ -1,4 +1,21 @@
 package com.reactivespring.handler;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
 public class ReviewHandler {
+
+    @Bean
+    public RouterFunction<ServerResponse> reviewsRoute() {
+
+        return route()
+                .GET("/v1/helloworld",(request -> ServerResponse.ok().bodyValue("helloworld")))
+                .build();
+    }
 }
