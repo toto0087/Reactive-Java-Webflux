b8f17599a7ef54d0b02a56883dddb08b73d84283
Tobias <tobiasocchi03@hotmail.com>
2023-07-06 11:06:45 -0300

    Functional put method

--- movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
+++ movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import java.nio.file.Path;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
@@ -15,9 +18,13 @@ public class ReviewRouter {
     public RouterFunction<ServerResponse> reviewsRoute(ReviewHandler reviewHandler) {
 
         return route()
+                .nest(path("/v1/helloworld"), builder -> {
+                    builder.POST("", request -> reviewHandler.addReview(request))
+                            .GET("", request -> reviewHandler.getReview(request))
+                            .PUT("/{id}",request -> reviewHandler.updateReview(request))
+                            .DELETE("/{id}",request -> reviewHandler.deleteReview(request));
+                })
                 .GET("/v1/helloworld",(request -> ServerResponse.ok().bodyValue("helloworld")))
-                .POST("/v1/reviews", request -> reviewHandler.addReview(request))
-                .GET("/v1/reviews", request -> reviewHandler.getReview(request))
                 .build();
     }
 }
